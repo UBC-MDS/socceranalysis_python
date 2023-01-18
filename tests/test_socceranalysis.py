a66b339cb0eea22c11565b7366e703d8de37e2d6
manvirsingh96 <manvirsk96@gmail.com>
2023-01-18 00:23:43 +0000

    Updated tests for get_outliers

--- tests/test_socceranalysis.py
+++ tests/test_socceranalysis.py
@@ -7,7 +7,7 @@ def test_get_outliers():
     assert len(get_outliers(data,"Market_Value_Euros","IQR")) == 328, "There should 328 outliers when using IQR on Market Value"
     assert len(get_outliers(data,"Wages_Euros","SD",3)) == 104, "There should 104 outliers when using SD method on Wages"
     assert len(get_outliers(data,"Wages_Euros","IQR")) == 324 , "There should 324 outliers when using IQR method on Wages"
-    assert len(get_outliers(data,"Wages_Euros","IQR").columns) == 32, "The outlier dataframe is incorrect (total columns should be 32)"
+    assert len(get_outliers(data,"Wages_Euros","IQR").columns) == 20, "The outlier dataframe is incorrect (total columns should be 20)"
     assert isinstance(data,pd.DataFrame) == True, "Input data should be a pandas dataframe"
     assert isinstance(get_outliers(data,"Wages_Euros","IQR"),pd.DataFrame) == True ,"Ouptut data should be a pandas dataframe"
     
